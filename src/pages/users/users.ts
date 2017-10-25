8629f7d32aa6e89c812f5850abed0f82b75f72d1
ajayns <ajay.ns@outlook.com>
2017-10-25 18:28:39 +0530

    Connect API to page

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { User } from '../../models/user';
+import { GithubUsersProvider } from '../../providers/github-users/github-users';
+
 @IonicPage()
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html',
 })
 export class UsersPage {
+  users: User[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private githubUsersProvider: GithubUsersProvider) {
+    
+    // Store users data from api to users
+    githubUsersProvider.load().subscribe(users => {
+      this.users = users;
+    })
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UsersPage');
-  }
 
 }
